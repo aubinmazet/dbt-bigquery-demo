8845362128baff5f76d1e69233fa9a929dad3cf2
aubinmazet <mazetaubin@gmail.com>
2021-12-09 15:36:01 +0100

    feat: add monthly aggregation table

--- dbt_demo/models/base/monthy_aggregation.sql
+++ dbt_demo/models/base/monthy_aggregation.sql
@@ -1,3 +1,14 @@
-select date_trunc('month', date) as month, sum(deces), sum(gueris)
-from {{ ref('covid_france') }}
+{{ 
+    config(
+        materialized='table'
+        ) 
+    
+}}
+
+select 
+    DATE_TRUNC(cast(date as date), MONTH) as month, 
+    sum(deces) as sum_deces, 
+    sum(gueris) as sum_gueris
+from covid.covid_france
 group by month
+order by month desc
